a79d2b87a4497f83a9c832a5a70868786edaa934
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-06-10 13:00:18 -0400

    Fixed swapped return values.

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,5 +3,5 @@ from asyncio.windows_events import NULL
 def validateThreshold(str):
     casted_int = int(str, 10)  # Convert the string to an integer
     if 0 <= casted_int <= 30:  # Check if the integer is within the valid range
-      return False
-    return casted_int
+      return casted_int
+    return False
